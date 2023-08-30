8dcca31ea0805e09e0c6ea286d39db8da442ce10
4z0t <gastec139@gmail.com>
2023-08-30 22:36:35 +0300

    Add 5th argunment

---
+++ section/IssueBuildMobileAlt.cpp
@@ -0,0 +1,19 @@
+void IssueBuildMobileFix()
+{
+    asm(
+        "mov     esi, [ebp+0x8];"
+        "mov     esi, [esi];"
+        "push    5;"
+        "push    esi;"
+        "call    0x90CA40;" // lua_toboolean
+        "add esp, 8;"
+        "test al, al;"
+        "jz PICK_NEAREST;"
+        "lea edx, [esp+0x94];"
+        "jmp UNIT_ISSUE;"
+        "PICK_NEAREST:"
+        "lea edx, [esp+0x6C];"
+        "UNIT_ISSUE:"
+        "call    0x6F12C0;"
+        "jmp     0x6F5FA0;");
+}
