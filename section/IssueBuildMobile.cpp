3468ac176f65ee965dd59bb48a6c6432ef8ac146
4z0t <gastec139@gmail.com>
2023-09-02 16:16:24 +0300

    refactor

--- section/IssueBuildMobile.cpp
+++ section/IssueBuildMobile.cpp
@@ -1,6 +1,7 @@
 void IssueBuildMobileFix()
 {
     asm(
+        "push    eax;"
         "push    5;"
         "push    esi;"
         "call    0x90CA40;" // lua_toboolean
@@ -8,10 +9,8 @@ void IssueBuildMobileFix()
         "test al, al;"
         "jz PICK_NEAREST;"
         "lea edx, [esp+0x94];"
-        "jmp UNIT_ISSUE;"
+        "jmp    0x6F5F9B;"//UNIT_ISSUE
         "PICK_NEAREST:"
         "lea edx, [esp+0x6C];"
-        "UNIT_ISSUE:"
-        "call    0x6F12C0;"
-        "jmp     0x6F5FA0;");
+        "jmp     0x6F5F9B;");
 }
