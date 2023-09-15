6941d8dec01f7141d7d5bea003813115e65fc1e1
Alexander <84857900+4z0t@users.noreply.github.com>
2023-09-15 16:04:02 +0300

    rename to control

--- section/SetAlpha.cpp
+++ section/SetAlpha.cpp
@@ -1,34 +1,34 @@
 #include "include/moho.h"
-void SetAlpha(int bitmap, float alpha, uint32_t alphai)
+void SetAlpha(int control, float alpha, uint32_t alphai)
 {
-    *(float *)(bitmap + 240) = alpha;
-    *(uint32_t *)(bitmap + 244) = (*(uint32_t *)(bitmap + 244) & 0x00FFFFFFu) | (alphai << 24);
+    *(float *)(control + 240) = alpha;
+    *(uint32_t *)(control + 244) = (*(uint32_t *)(control + 244) & 0x00FFFFFFu) | (alphai << 24);
 }
 
-int __thiscall _SetAlpha(int bitmap, float alpha)
+int __thiscall _SetAlpha(int control, float alpha)
 {
     uint32_t alphai = 0xFF * alpha;
     if (alphai > 0xFF)
     {
         alphai = 0xFF;
     }
-    SetAlpha(bitmap, alpha, alphai);
+    SetAlpha(control, alpha, alphai);
     return alphai;
 }
 
 int __fastcall NextChild(int child, int parent) asm("0x786EA0");
 
-void __thiscall _SetAlphaChildren(int bitmap, float alpha)
+void __thiscall _SetAlphaChildren(int control, float alpha)
 {
     uint32_t alphai = 0xFF * alpha;
     if (alphai > 0xFF)
     {
         alphai = 0xFF;
     }
-    int child = bitmap;
+    int child = control;
     do
     {
         SetAlpha(child, alpha, alphai);
-        child = NextChild(child, bitmap);
+        child = NextChild(child, control);
     } while (child);
 }
