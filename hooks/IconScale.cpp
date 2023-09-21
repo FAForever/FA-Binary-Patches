f11829091e9d535b8abbc9b5b95800159b01ab1a
4z0t <gastec139@gmail.com>
2023-09-21 12:31:14 +0300

    scale status icons

--- hooks/IconScale.cpp
+++ hooks/IconScale.cpp
@@ -3,5 +3,12 @@ asm(
     ".section h0; .set h0,0x85DE05;"
     "jmp "QU(ScaleIcons)";"
     "nop;"
+    "nop;" 
+    ".section h1; .set h1,0x85DF02;"
+    "jmp "QU(ScalePauseIcon)";"
+    "nop;"
+    "nop;"
+    ".section h2; .set h2,0x85E003;"
+    "jmp "QU(ScaleStunIcon)";"
     "nop;"
 );
--- section/IconScale.cpp
+++ section/IconScale.cpp
@@ -1,4 +1,4 @@
-#include <magic_classes.h>
+#include "include/magic_classes.h"
 
 float iconscale = 1.0;
 ConDescReg icons_scale_var{"ui_StrategicIconScale", "", &iconscale};
@@ -15,3 +15,29 @@ void ScaleIcons()
         : [iconscale] "i"(&iconscale)
         :);
 }
+void ScalePauseIcon()
+{
+    asm(
+        "movaps  xmm0, xmm2;"
+        "mulss xmm4, ds:%[iconscale];"
+        "mulss xmm5, ds:%[iconscale];"
+        "addss   xmm0, xmm4;"
+        "jmp 0x85DF09;"
+        :
+        : [iconscale] "i"(&iconscale)
+        :);
+}
+
+void ScaleStunIcon()
+{
+    asm(
+        "movaps  xmm0, xmm3;"
+        "movaps  xmm1, xmm2;"
+        "mulss xmm5, ds:%[iconscale];"
+        "mulss xmm6, ds:%[iconscale];"
+        "jmp 0x85E009;"
+        :
+        : [iconscale] "i"(&iconscale)
+        :);
+}
+
