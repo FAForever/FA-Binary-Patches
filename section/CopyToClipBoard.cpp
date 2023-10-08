5bd40efeb8ef57642418a960ae652d763e3bf7fc
4z0t <gastec139@gmail.com>
2023-10-08 19:02:38 +0300

    implement CopyToClipboard

---
+++ section/CopyToClipBoard.cpp
@@ -0,0 +1,24 @@
+#include "include/CObject.h"
+#include "include/magic_classes.h"
+#include "include/moho.h"
+#include "include/utility.h"
+#include <cmath>
+
+bool __cdecl WIN_CopyToClipboard(const wchar_t *Src) asm("0x004F2730");
+
+wstring *__cdecl wstring_copy_ctor(wstring *ws, const char *s) asm("0x938720");
+
+int UI_CopyToClipboard(lua_State *l)
+{
+    if (lua_gettop(l) != 1)
+    {
+        l->LuaState->Error(s_ExpectedButGot, __FUNCTION__, 1, lua_gettop(l));
+    }
+    const char *s = lua_tostring(l, 1);
+    wstring ws;
+    wstring_copy_ctor(&ws, s);
+    lua_pushboolean(l, WIN_CopyToClipboard(ws.data()));
+    return 1;
+}
+
+UIRegFunc copyToClipboardReg{"UI_CopyToClipboard", "", UI_CopyToClipboard};
