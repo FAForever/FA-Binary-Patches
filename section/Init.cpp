b2b10a07398f90574d657c6c40504ed5ea1c3287
4z0t <gastec139@gmail.com>
2024-11-12 13:58:11 +0300

    proper types

--- section/Init.cpp
+++ section/Init.cpp
@@ -1,10 +1,12 @@
 #include "moho.h"
 
-extern const uintptr_t FIRST_CTOR;
-extern const uintptr_t END_CTOR;
+typedef void (*global_ctor)();
+extern const global_ctor FIRST_CTOR;
+extern const global_ctor END_CTOR;
 
-void InitCtors() {
-    for(auto iter = &FIRST_CTOR; iter != &END_CTOR; ++iter)
+void InitCtors()
+{
+    for (auto iter = &FIRST_CTOR; iter != &END_CTOR; ++iter)
         if (iter != nullptr)
-            (reinterpret_cast<void(*)()>(*iter))();
+            (*iter)();
 }
