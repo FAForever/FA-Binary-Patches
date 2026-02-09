5380140dacebdab3473f3bfe6e2903ed957c97dc
Strogoo <36897892+Strogoo@users.noreply.github.com>
2026-02-09 22:53:04 +0300

    Update section/PathfindingTweaks.cpp
    Co-authored-by: Alexander <84857900+4z0t@users.noreply.github.com>

--- section/PathfindingTweaks.cpp
+++ section/PathfindingTweaks.cpp
@@ -6,17 +6,6 @@
 
 float navPersonalPosMaxDistance = 50.0;
 
-volatile void asm__PersonalPositioningDistanceCheck()
-{
-    asm(
-        "fld %[navPersonalPosMaxDistance];"
-        "jmp 0x005AF8DB;"
-        
-        :
-        : NON_GENERAL_REG(navPersonalPosMaxDistance)
-        : 
-    );
-}
 
 int SetNavIndividualPosMaxDistance(lua_State *L) {
     navPersonalPosMaxDistance = luaL_optnumber(L, 1, 50.0);
