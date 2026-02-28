372b769c40ca3e41179d69d299914a1254df2240
Alexander <gastec139@gmail.com>
2026-02-28 12:51:50 +0300

    blip fix

--- section/Aim/Aim.cxx
+++ section/Aim/Aim.cxx
@@ -17,13 +17,29 @@ namespace Moho
     };
 }
 
+
+// When targeted manually it uses blip and when not - unit
 SHARED __thiscall bool CheckNeedAddEntityVelocity(Moho::CAiTarget *cai_target)
 {
     void *entity = cai_target->entity;
     if (entity == 0 || entity == (void *)4)
         return false;
 
-    entity = Offset<void *>(entity, -4);
+    entity = Offset(entity, -4);
+
+    void *vtable = GetField(entity, 0x0);
+    void *__thiscall (*IsBlip)(void *) = GetField<void *__thiscall (*)(void *entity)>(vtable, 28);
+    void *blip = IsBlip(entity);
+
+    if (blip)
+    {
+        void *unit_creator_ptr = GetField<void *>(blip, 0x270);
+        if (unit_creator_ptr == nullptr)
+            return false;
+        void *unit_creator = Offset(unit_creator_ptr, -4);
+        entity = Offset(unit_creator, 8);
+    }
+
     int update_tick = GetField<int>(entity, 0x174);
     void *sim = GetField<void *>(entity, 0x148);
     int sim_tick = GetField<int>(sim, 0x900);
@@ -42,11 +58,12 @@ SHARED __thiscall float AimAddVelocity(
     const Vector3f &target_velocity,
     const Vector3f &target_pos)
 {
-    if (CheckNeedAddEntityVelocity(cai_target) && cai_target->HasTargetPoint())
+    if (CheckNeedAddEntityVelocity(cai_target))
         aim_pos = target_pos + target_velocity;
     else
         aim_pos = target_pos;
 
+    // aim_pos = target_pos + target_velocity;
     return std::sqrt(
         (aim_pos.x - shooter_pos.x) * (aim_pos.x - shooter_pos.x) +
         // (aim_pos.y - shooter_pos.y) * (aim_pos.y - shooter_pos.y) +
