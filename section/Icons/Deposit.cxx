2dc1f3adefc960073b0386bee5ba184b9fe2dda6
Alexander <gastec139@gmail.com>
2026-04-10 19:40:27 +0300

    rename to deposit

---
+++ section/Icons/Deposit.cxx
@@ -0,0 +1,4 @@
+#include "magic_classes.h"
+
+SHARED float deposit_icon_scale = 1.0;
+ConDescReg deposit_scale_var{"ui_DepositIconScale", "", &deposit_icon_scale};
--- section/Icons/Resources.cxx
+++
@@ -1,4 +0,0 @@
-#include "magic_classes.h"
-
-SHARED float resources_icon_scale = 1.0;
-ConDescReg res_scale_var{"ui_ResourcesIconScale", "", &resources_icon_scale};
