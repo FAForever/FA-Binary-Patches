dbc1c9da13ebb990e8b8dae0e53cabe24c9d08d4
Alexander <gastec139@gmail.com>
2026-05-08 15:04:22 +0300

    more fitting name

--- section/Minimap/Minimap.cxx
+++ section/Minimap/Minimap.cxx
@@ -1,4 +1,4 @@
 #include "magic_classes.h"
 
 SHARED bool render_minimap_ranges = true;
-ConDescReg<bool> ui_RenderMinimapRanges_var("ui_RenderMinimapRanges", "", &render_minimap_ranges);
+ConDescReg<bool> ren_MinimapRanges_var("ren_MinimapRanges", "", &render_minimap_ranges);
